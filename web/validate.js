66ce2ff8ee39a7fc8695b4e3f5263c27798244c9
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-01-26 22:08:35 -0600

    Rewrite of functions

--- web/validate.js
+++ web/validate.js
@@ -8,149 +8,158 @@ Must match the policy on the page and the policy checks in the other scripts
 */
 
 
-function LengthCheck(target_id, min, max) {
-	var pw = document.getElementById(target_id).value.length;
-	return pw >= min && pw <= max;
+function LengthCheck(string, min, max) {
+	var str_length = string.length;
+	return str_length >= min && str_length <= max;
 }
-function UppercaseCheck(min_number) {
-	var pw = document.getElementById("password").value;
-	return pw.length - pw.replace(/[A-Z]/g, '').length >= min_number;
+function UppercaseCheck(string, min_number) {
+	return string.length - string.replace(/[A-Z]/g, "").length >= min_number;
 }
-function LowercaseCheck(min_number) {
-	var pw = document.getElementById("password").value;
-	return pw.length - pw.replace(/[a-z]/g, '').length >= min_number;
+function LowercaseCheck(string, min_number) {
+	return string.length - string.replace(/[a-z]/g, "").length >= min_number;
 }
-function DigitCheck(min_number) {
-	var pw = document.getElementById("password").value;
-	return pw.length - pw.replace(/[0-9]/g, '').length >= min_number;
+function DigitCheck(string, min_number) {
+	return string.length - string.replace(/[0-9]/g, "").length >= min_number;
 }
-function SymbolCheck(min_number) {
-	var pw = document.getElementById("password").value;
-	return pw.length - pw.replace(/[!@#$%^&*]/g, '').length >= min_number;
+function SymbolCheck(string, min_number) {
+	return string.length - string.replace(/[!@#$%^&*]/g, "").length >= min_number;
 }
-function HasForbiddenCharCheck() {
-	var pw = document.getElementById("password").value;
-	return pw.replace(/[A-Za-z0-9!@#$%^&*]/g, '').length > 0;
+function HasForbiddenCharCheck(string) {
+	return string.replace(/[A-Za-z0-9!@#$%^&*]/g, "").length > 0;
 }
-function HasForbiddenUsernameCheck() {
-	var user = document.getElementById("new-username").value;
-	return user.replace(/[A-Za-z0-9]/g, '').length > 0 && user != "";
+function HasForbiddenUsernameCheck(string) {
+	return string.replace(/[A-Za-z0-9]/g, "").length > 0 && string != "";
 }
-
-function PasswordsMatch() {
-	return document.getElementById("password").value == document.getElementById("confirm-password").value;
-}
-function DeleteUsernameMatch() {
-	return document.getElementById("delete-username").value == document.getElementById("confirm-delete-username").value;
+function HasMatchingStrings(string1, string2) {
+	return string1 == string2;
 }
 
 function UsernameFeedback() {
+    var new_username_field = document.getElementById("new-username");
+
 	// Feedback when the users enters a bad character
-	if (HasForbiddenUsernameCheck()) {
-		document.getElementById('new-username').classList.add('is-invalid');
+	if (HasForbiddenUsernameCheck(new_username_field.value)) {
+		new_username_field.classList.add('is-invalid');
 		document.getElementById('username-rule-no-weird').classList.remove('d-none');
 	} else {
-		document.getElementById('new-username').classList.remove('is-invalid');
+		new_username_field.classList.remove('is-invalid');
 		document.getElementById('username-rule-no-weird').classList.add('d-none');
 	}
 	// Check username length
-	if (LengthCheck("new-username", 5, 50)) { document.getElementById('username-rule-length').classList.add('d-none'); } else { document.getElementById('username-rule-length').classList.remove('d-none'); }
+	if (LengthCheck(new_username_field.value, 5, 50)) { document.getElementById('username-rule-length').classList.add('d-none'); } else { document.getElementById('username-rule-length').classList.remove('d-none'); }
 	// The previous code shows and hides rules if things are met. If everything is hidden, assume all is good.
 	if (document.querySelectorAll("#username-policy li:not(.d-none)").length == 1) { // The requirement on unique can't be checked, its just a note to the user.
-		document.getElementById('new-username').classList.add('is-valid');
+		new_username_field.classList.add('is-valid');
 		document.getElementById('username-policy').classList.add('d-none');
 	} else {
-		document.getElementById('new-username').classList.remove('is-valid');
+		new_username_field.classList.remove('is-valid');
 		document.getElementById('username-policy').classList.remove('d-none');
 	}
 }
 
 function PasswordPolicyFeedback() {
+    var password_field = document.getElementById("password");
+
 	// Hide using d-none (display:none from bootstrap) when condi is met.
-	if(LengthCheck("password", 12, 32)) { document.getElementById('password-rule-length').classList.add('d-none'); } else { document.getElementById('password-rule-length').classList.remove('d-none'); }
-	if(UppercaseCheck(1)) { document.getElementById('password-rule-uppercase').classList.add('d-none'); } else { document.getElementById('password-rule-uppercase').classList.remove('d-none'); }
-	if(LowercaseCheck(1)) { document.getElementById('password-rule-lowercase').classList.add('d-none'); } else { document.getElementById('password-rule-lowercase').classList.remove('d-none'); }
-	if(SymbolCheck(1)) { document.getElementById('password-rule-symbol').classList.add('d-none'); } else { document.getElementById('password-rule-symbol').classList.remove('d-none'); }
-	if(DigitCheck(1)) { document.getElementById('password-rule-digit').classList.add('d-none'); } else { document.getElementById('password-rule-digit').classList.remove('d-none'); }
+	if(LengthCheck(password_field.value, 12, 32)) { document.getElementById('password-rule-length').classList.add('d-none'); } else { document.getElementById('password-rule-length').classList.remove('d-none'); }
+	if(UppercaseCheck(password_field.value, 1)) { document.getElementById('password-rule-uppercase').classList.add('d-none'); } else { document.getElementById('password-rule-uppercase').classList.remove('d-none'); }
+	if(LowercaseCheck(password_field.value, 1)) { document.getElementById('password-rule-lowercase').classList.add('d-none'); } else { document.getElementById('password-rule-lowercase').classList.remove('d-none'); }
+	if(SymbolCheck(password_field.value, 1)) { document.getElementById('password-rule-symbol').classList.add('d-none'); } else { document.getElementById('password-rule-symbol').classList.remove('d-none'); }
+	if(DigitCheck(password_field.value, 1)) { document.getElementById('password-rule-digit').classList.add('d-none'); } else { document.getElementById('password-rule-digit').classList.remove('d-none'); }
 	
 	// Feedback when the users enters a bad character
-	if (HasForbiddenCharCheck()) {
-		document.getElementById('password').classList.add('is-invalid');
+	if (HasForbiddenCharCheck(password_field.value)) {
+		password_field.classList.add('is-invalid');
 		document.getElementById('password-rule-no-weird').classList.remove('d-none');
 	} else {
-		document.getElementById('password').classList.remove('is-invalid');
+		password_field.classList.remove('is-invalid');
 		document.getElementById('password-rule-no-weird').classList.add('d-none');
 	}
 	
 	// The previous code shows and hides rules if things are met. If everything is hidden, assume all is good.
 	if (document.querySelectorAll("#password-policy li:not(.d-none)").length == 0) {
-		document.getElementById('password').classList.add('is-valid');
+		password_field.classList.add('is-valid');
 		document.getElementById('password-policy').classList.add('d-none');
 	} else {
-		document.getElementById('password').classList.remove('is-valid');
+		password_field.classList.remove('is-valid');
 		document.getElementById('password-policy').classList.remove('d-none');
 	}
 }
 
 function CheckMatchingPasswordsFeedback() {
-	if (PasswordsMatch()) {
-		document.getElementById('confirm-password').classList.add('is-valid');
+    var password_field = document.getElementById('password');
+    var password_confirm_field = document.getElementById('confirm-password');
+
+	if (HasMatchingStrings(password_field.value, password_confirm_field.value)) {
+		password_confirm_field.classList.add('is-valid');
 	} else {
-		document.getElementById('confirm-password').classList.remove('is-valid');
+		password_confirm_field.classList.remove('is-valid');
 	}
 }
 
 function CheckDeleteUsernameFeedback() {
-	if (DeleteUsernameMatch()) {
-		document.getElementById('delete-username').classList.add('is-valid');
-		document.getElementById('confirm-delete-username').classList.add('is-valid');
+    var delete_user_field = document.getElementById('delete-username');
+    var confirm_delete_user_field = document.getElementById('confirm-delete-username');
+
+	if (HasMatchingStrings(delete_user_field.value, confirm_delete_user_field.value)) {
+		delete_user_field.classList.add('is-valid');
+		confirm_delete_user_field.classList.add('is-valid');
 	} else {
-		document.getElementById('delete-username').classList.remove('is-valid');
-		document.getElementById('confirm-delete-username').classList.remove('is-valid');
+		delete_user_field.classList.remove('is-valid');
+		confirm_delete_user_field.classList.remove('is-valid');
 	}
 }
 
 function ValidateAddUserForm() {
-	if (HasForbiddenUsernameCheck()) {
+    var new_username_field = document.getElementById("new-username");
+
+	if (HasForbiddenUsernameCheck(new_username_field.value)) {
 		alert("Username contains forbidden characters.");
 	}
 	ValidateChangePasswordForm();
 }
 
 function ValidateDeleteUserForm() {
-	if (DeleteUsernameMatch() == false) {
+    var delete_user_field = document.getElementById('delete-username');
+    var confirm_delete_user_field = document.getElementById('confirm-delete-username');
+
+	if (HasMatchingStrings(delete_user_field.value, confirm_delete_user_field.value) == false) {
 		alert("The given username to delete fields do not match.");
 		return false;
 	}
 }
 
 function ValidateChangePasswordForm() {
-	if (PasswordsMatch() == false) {
+    var password_field = document.getElementById("password");
+    var password_confirm_field = document.getElementById('confirm-password');
+
+	if (HasMatchingStrings(password_field.value, password_confirm_field.value) == false) {
 		alert("Passwords don't match.");
 		return false;
 	}
-	if (HasForbiddenCharCheck()) {
+	if (HasForbiddenCharCheck(password_field.value)) {
 		alert("Forbidden characters in password.");
 		return false;
 	}
-	if (LengthCheck(12, 32) == false) {
+	if (LengthCheck(password_field.value, 12, 32) == false) {
 		alert("Bad password length.");
 		return false;
 	}
-	if (UppercaseCheck(1) == false) {
+	if (UppercaseCheck(password_field.value, 1) == false) {
 		alert("Password does not have enough uppercase letters.");
 		return false;
 	}
-	if (LowercaseCheck(1) == false) {
+	if (LowercaseCheck(password_field.value, 1) == false) {
 		alert("Password does not have enough lowercase letters.");
 		return false;
 	}
-	if (SymbolCheck(1) == false) {
+	if (SymbolCheck(password_field.value, 1) == false) {
 		alert("Password does not have enough symbols.");
 		return false;
 	}
-	if (DigitCheck(1) == false) {
+	if (DigitCheck(password_field.value, 1) == false) {
 		alert("Password does not have enough digits.");
 		return false;
 	}
 }
+
