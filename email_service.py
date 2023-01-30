cf94be0d5683847ea6facc8c1d034d5443563aa5
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-01-29 21:17:22 -0600

    Update email_service.py
    formatting fixes

--- email_service.py
+++ email_service.py
@@ -21,21 +21,22 @@ msg_type:
 account_name: string
 
  """
+import json  # To help read the mailslurp error message
 import os  # Used to get the .env file
-import mailslurp_client
-from smtplib import SMTP
-from dotenv import load_dotenv  # Used to load info from the .env file
 import smtplib
 from email.mime.text import MIMEText
-import json  # To help read the mailslurp error message
+from smtplib import SMTP
+
+import mailslurp_client
+from dotenv import load_dotenv  # Used to load info from the .env file
 
 
 def mailslurpt_send_email(subject_string, message_string):
     try:
         load_dotenv()  # Load the secrets from the .env file
-        api_key = os.environ.get('MAILSLURP_API_KEY')
-        sender_id = os.environ.get('MAILSLURP_SENDER_EMAIL_ID')
-        receiver_id = os.environ.get('MAILSLURP_RECIEVER_EMAIL_ID')
+        api_key = os.environ.get("MAILSLURP_API_KEY")
+        sender_id = os.environ.get("MAILSLURP_SENDER_EMAIL_ID")
+        receiver_id = os.environ.get("MAILSLURP_RECIEVER_EMAIL_ID")
 
         # Check for the keys
         assert "MAILSLURP_API_KEY" in os.environ
@@ -44,19 +45,19 @@ def mailslurpt_send_email(subject_string, message_string):
 
         # create a mailslurp configuration
         configuration = mailslurp_client.Configuration()
-        configuration.api_key['x-api-key'] = api_key
+        configuration.api_key["x-api-key"] = api_key
 
         with mailslurp_client.ApiClient(configuration) as api_client:
             # create an SMTP inbox
             inbox_controller = mailslurp_client.InboxControllerApi(api_client)
             system_email = inbox_controller.get_inbox(sender_id)
             admin_email = inbox_controller.get_inbox(receiver_id)
-            #print(system_email)
-            #print(admin_email)
+            # print(system_email)
+            # print(admin_email)
 
             # get smtp imap access
             smtp_access = inbox_controller.get_imap_smtp_access(inbox_id=system_email.id)
-            msg = "Subject: "+subject_string+"\r\n\r\n"+message_string
+            msg = "Subject: " + subject_string + "\r\n\r\n" + message_string
 
             with SMTP(host=smtp_access.smtp_server_host, port=smtp_access.smtp_server_port) as smtp:
                 smtp.login(user=smtp_access.smtp_username, password=smtp_access.smtp_password)
@@ -64,7 +65,7 @@ def mailslurpt_send_email(subject_string, message_string):
                 smtp.quit()
 
     except AssertionError:
-        print('Missing or incorrectly configured .env file')
+        print("Missing or incorrectly configured .env file")
     except mailslurp_client.ApiException as x:
         # The mailslurp_client.ApiException returns a body with json format
         # Read back the message to know which is the issue.
@@ -97,19 +98,21 @@ def send_email(email_system, msg_type, account_name):
     message = "default message"
 
     if msg_type == 1:
-        print('user account locked')
+        print("user account locked")
         email_title = f"[System] Account named {account_name} as been locked for suspicious activity"
-        message = f"Dear Admin,\nThe account named {account_name} has made 10 bad attempts to get into their account. " \
-                  f"The system has locked {account_name}."
+        message = (
+            f"Dear Admin,\nThe account named {account_name} has made 10 bad attempts to get into their account. "
+            f"The system has locked {account_name}."
+        )
     elif msg_type == 2:
-        print('system breach')
+        print("system breach")
         email_title = f"[ALERT] A decoy password has been used for {account_name}"
-        message = f"Dear Admin,\nA decoy password for user {account_name} has been used." \
-                  f"The database might be breached."
+        message = (
+            f"Dear Admin,\nA decoy password for user {account_name} has been used." f"The database might be breached."
+        )
     if email_system == "test":
         # sender_email, recipient_email are made up because they don't really exist beyond the DebuggingServer
         localhost_send_email("no-reply@example.com", "admin@example.com", email_title, message)
     elif email_system == "live":
         # Use sparingly
         mailslurpt_send_email(email_title, message)
-
