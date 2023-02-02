28b84a0ad009583d52f3fac3b02bf47935bd4acc
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-02-01 20:53:53 -0600

    Update test_example_passwords.py
    Removed todo

--- tests/test_example_passwords.py
+++ tests/test_example_passwords.py
@@ -5,7 +5,6 @@ This just makes sure you haven't added something to the wrong file
 from password_checker import password_valid_to_policy_rules
 
 
-# TODO: THIS WILL BLOW UP ON THE CI IF YOU FORGET YOUR FILES WHICH YOU HAVE IGNORED
 class TestPasswordExamples:
 	def test_is_valid(self):
 		with open("../examples/example_valid_passwords.txt") as file:
