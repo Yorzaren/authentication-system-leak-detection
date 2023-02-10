c83f3cef57b449702a139f92a77fc6fdc0209c21
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-02-10 17:39:36 -0600

    Probably as good as it will get on IDing a random string
    The code will still ID strings that aren't random as random.

--- password_analysis_helper.py
+++ password_analysis_helper.py
@@ -82,14 +82,17 @@ def convert_from_leet(string: str) -> str:
 
 # TODO: I haven't found a foolproof way to calc it, consider other methods.
 # The goal of this function is to detect if the string is random letters, symbols, and numbers
-# You already tried https://github.com/casics/nostril
-# which will fail if the sample is too small or gives both false positives and negatives.
+# This use https://github.com/casics/nostril as an import and nltk
+# Nostril will fail if the sample is too small or gives both false positives and negatives.
+# This still have false positives. (It will sometimes say a string is random when it isn not)
 def is_random_string(string: str, debugging=False) -> bool:
+    RANDOMNESS_THRESHOLD = 0  # Default is 0
+    VIEW_SCOREING = False  # This is for super debugging, so I can track the where the score is coming from.
     randomness_score = 0
     unleeted_string = convert_from_leet(string)
     capital_split_array = split_capital_strings(string)
-    pp_capital_split_array_unleeted = []
-    pp_symbols_capital_split_array = []
+    pp_capital_split_array_unleeted = []  # Hold strings after capital split + unleet
+    pp_symbols_capital_split_array = []  # Hold strings after capital split + unleet + symbols split
 
     # This is to deal with C@pta1nC@veM4n like strings because they will be seen as  C@pta1n C@ve M4n and not words
     for entry in capital_split_array:
@@ -106,11 +109,16 @@ def is_random_string(string: str, debugging=False) -> bool:
 
     # Process it again to remove other undesirables
     for entry in pp_capital_split_array_unleeted:
-        pp_symbols_capital_split_array.append(entry)
+        split_string = re.split(r'[!@#$%^&*]+', entry)
+        for x in split_string:
+            pp_symbols_capital_split_array.append(x)
 
-    print("capital_split_array" + str(capital_split_array))
-    print("pp_capital_split_array_unleeted" + str(pp_capital_split_array_unleeted))
-    print("post-processing symbol removal + unleeting capital_split_array : " + str(pp_symbols_capital_split_array))
+    if debugging is True:
+        print('\n---- Start Array Info ----')
+        print("capital_split_array : " + str(capital_split_array))
+        print("pp_capital_split_array_unleeted : " + str(pp_capital_split_array_unleeted))
+        print("pp_symbols_capital_split_array : " + str(pp_symbols_capital_split_array))
+        print('----  End Array Info  ----\n')
 
 
     string_array = split_letter_strings(string)
@@ -132,59 +140,77 @@ def is_random_string(string: str, debugging=False) -> bool:
 
     if size_string_array == size_unleeted_array:
         # If the array sizes don't change its probably random.
-        # print("-->Didn't Shrink")
+        if debugging is True:
+            print("-->Didn't Shrink")
         randomness_score += 2
+        if VIEW_SCOREING is True:
+            print("+2")
+
     if len(max_len_unleeted_array) >= len(string)/2:
         # If the unleeted_string is half or more it's not really random... ?
-        # print("-->The string seems to be too long, so prob not random")
+        if debugging is True:
+            print("-->The string seems to be too long, so prob not random")
         randomness_score -= 2
+        if VIEW_SCOREING is True:
+            print("-2")
 
     # Combine arrays
     merged_array = []
-    for elem in string_array + unleeted_array + capital_split_array + pp_capital_split_array_unleeted:
+    for elem in string_array + unleeted_array + capital_split_array + pp_capital_split_array_unleeted + pp_symbols_capital_split_array:
         normalize_case = elem.lower()
         if normalize_case not in merged_array:
             merged_array.append(normalize_case)
 
-    print(str(merged_array))
+    #print(str(merged_array))
 
     # Check the array
     for x in merged_array:
-        if len(x) >= 2:  # Skip words less than 2-letters long
-            potential_word = x.lower()
+        potential_word = x.lower()
+        if len(potential_word) > 2:  # Skip words less than 2-letters long
 
             # Use nltk to check for the array for dictionary words
             if potential_word in set_of_words:
-                print("-->Real word?: " + potential_word)
+                if debugging is True:
+                    print("-->Real word?: " + potential_word)
+
                 word_length = len(potential_word)
+
                 if word_length == 2:
                     randomness_score -= 1
+                    if VIEW_SCOREING is True:
+                        print("-1")
                 elif word_length == 3 or word_length == 4:
                     randomness_score -= 2
+                    if VIEW_SCOREING is True:
+                        print("-2")
                 else:  # more than 5-letter word that can't be random mistake
                     randomness_score -= 5
+                    if VIEW_SCOREING is True:
+                        print("-5")
             else:
                 randomness_score += 0.2
+                if VIEW_SCOREING is True:
+                    print("+0.2")
             # End nltk check
 
-    # This doesnt seem to have any impact...
-    """
+    # This seems to have more of an impact on random strings
     # Use nostril check randomness - it does have false pos and false negs tho.
     try:
         if nonsense(unleeted_string):
-            # If its close send it over the edge
-            print("Is NostrilNonsense: " + str(nonsense(unleeted_string)))
+            if debugging is True:
+                print("Is NostrilNonsense: " + str(nonsense(unleeted_string)))
             randomness_score += 2
-    except:
+            if VIEW_SCOREING is True:
+                print("+2")
+    except:  # Have this here, so it doesn't blow up when the string is too short.
         pass
-    """
 
     if debugging is True:
-        print("Random Score: " + str(randomness_score) )
-        print("IsRandom: " + str(randomness_score >= 0) )
+        print("Random Score: " + str(randomness_score))
+        print("IsRandom: " + str(randomness_score >= RANDOMNESS_THRESHOLD))
         print('\n-----------------\n')
 
-    return randomness_score >= 0
+    return randomness_score >= RANDOMNESS_THRESHOLD
 
 
 
@@ -192,15 +218,24 @@ def is_random_string(string: str, debugging=False) -> bool:
 
 
 if __name__ == '__main__':
+    pass_s = [
+        "9Sk5yRyY2^8D",
+        "PoLHrs#jG!a#zaU!zXPbfSCk%an4#6yx",
+        "M8wyg5sYg8P^L9",
+        "WDcrQBnAtfDtn4Tg$#W96At86tJh%t3F"
+    ]
     pass_test = ["1Am4Gr8C0d3r",
                  "D32$jr#Q^VpD",
                  "sdfsdfsWEW34!",
                  "D0g!is!great!p3t",
                  "sundayPizz4Party!",
                  "Iamtheendandthebeginning!1"]
+
     with open("examples/example_random_passwords.txt") as file:
         for line in file:
             pass_test.append(line.strip())
     for s in pass_test:
-        is_random_string(s)
+        if is_random_string(s) is False:
+            print(s)
+
 
--- regression_check.py
+++ regression_check.py
@@ -2,12 +2,13 @@
 This shouldn't be used as a production test because it can through a lot of issues
 This is just to test if things work more or less.
 """
+import pytest
 
-"""
 from password_analysis_helper import is_random_string
 
 
 class TestPasswordAnalysisRegressionCheck:
+    @pytest.mark.skip(reason="Don't run this on production")
     def test_real_random_passwords(self):
         random_passwords = []
         with open("examples/example_random_passwords.txt") as file:
@@ -16,6 +17,8 @@ class TestPasswordAnalysisRegressionCheck:
         for s in random_passwords:
             assert is_random_string(s) is True
 
+# This part will likely bug out
+    @pytest.mark.skip(reason="This will bug out")
     def test_valid_passwords(self):
         random_passwords = []
         with open("examples/example_valid_passwords.txt") as file:
@@ -23,4 +26,3 @@ class TestPasswordAnalysisRegressionCheck:
                 random_passwords.append(line.strip())
         for s in random_passwords:
             assert is_random_string(s) is False
-"""
