17518247f4f4c4d34e1b1ff26159f85ea669fde8
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-03-07 14:22:42 -0600

    Working on login
    Anon Users will break things.

--- app.py
+++ app.py
@@ -6,31 +6,146 @@ Components are stored in `static` and `templates`
 
 
 """
+from flask import Flask, render_template, request, redirect, url_for, flash
+import flask_login
 
-from flask import Flask, render_template, request
+from python_scripts.database_controller import is_admin, user_exists
+from python_scripts.main import (
+    add_user_account,
+    delete_user,
+    is_authenticated,
+    unlock_account,
+    update_password,
+)
 
 app = Flask(__name__)
+app.secret_key = 'secret'  # Change this!
+
+# Login Related
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+# Define User Class
+class User():
+    def __init__(self, id, admin=False):
+        self.id = id
+        self.admin = admin
+
+    def is_active(self):
+        # Here you should write whatever the code is
+        # that checks the database if your user is active
+        # return self.active
+        # for demo i just return True
+        return True
+
+    def is_authenticated(self):
+        # for demo i just return True
+        return True
+
+    def get_id(self):
+        # if you do not use Usermixin, this is important
+        # user_loader load_user(id) uses this get_id attribute to load the id
+        return self.id
+
+
+@login_manager.user_loader
+def user_loader(username):
+    if user_exists(username) is False:
+        return
+
+    user = User(username)
+    user.id = username
+    user.admin = is_admin(username)
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    if user_exists(username) is False:
+        return
+
+    user = User(username)
+    user.id = username
+    user.admin = is_admin(username)
+    return user
+
+
+"""if session is not None:
+    # Get the session data is it exists
+    if "username" in session:
+        current_username = session["username"]
+    if "signed_in" in session:
+        is_signed_in = session["signed_in"]
+    if "admin" in session:
+        user_is_admin = session["is_admin"]
+"""
 
 
 @app.route("/")
 @app.route('/index')
 def index():
-    return render_template("index.html", title="Demo - Password F.M.")
+    # If they aren't logged in, redirect them.
+    if not flask_login.current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template("index.html", title="Index", username=flask_login.current_user.id, admin=flask_login.current_user.admin)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html", title="Login")
+
+    # Get the info from the form
+    username = request.form['username']
+    pw = request.form['password']
+
+    if is_authenticated(username, pw):  # Check it
+        # Assign
+        user = User(username)
+        # Set User Info
+        user.id = username
+        user.admin = is_admin(username)
+        flask_login.login_user(user)
+        # Return them to /index
+        flash('Login successful')
+        return redirect(url_for('index'))
+
+    return 'Bad login'
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About - Password F.M.", is_signed_in=True, is_admin=True)
+    return render_template("about.html", title="About", username=flask_login.current_user.id)
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as admin account: ' + str(flask_login.current_user.admin)
 
 
 @app.route("/admin")
+@flask_login.login_required
 def admin():
-    return render_template("admin.html", title="About - Password F.M.", is_signed_in=True, is_admin=True)
+    if flask_login.current_user.admin is True:
+        return render_template("admin.html", title="Admin", username=flask_login.current_user.id)
+    else:
+        return 'You can not be here'
 
 
 @app.route("/settings")
+@flask_login.login_required
 def settings():
-    return render_template("settings.html", title="About - Password F.M.", is_signed_in=True, is_admin=True)
+    return render_template("settings.html", title="Settings", username=flask_login.current_user.id)
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return render_template("logout.html", title="Logged Out")
 
 
 @app.errorhandler(404)
